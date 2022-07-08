212f00e5b3400cc71bf8c31cb6ed6ff5233feba7
777v56 <107239556+777v56@users.noreply.github.com>
2022-07-08 11:09:21 +0000

    2nd

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -18,19 +18,33 @@ class BooksController < ApplicationController
   end
   
   def update
-    list = Book.find(params[:id])
-    list.update(book_params)
-    redirect_to book_path(book.id)  
+    @book = Book.find(params[:id])
+    if @book.update(book_params)
+      flash[:notice] = "Book was successfully created."
+      redirect_to book_path(book.id)  
+    else
+      @books = Book.all
+      render :edit
+    end
   end
   
   def create
-    book = Book.new(book_params)
-    book.save
-    redirect_to book_path(book.id)  
+    @book = Book.new(book_params)
+    if @book.save
+      flash[:notice] = "Book was successfully created."
+      redirect_to book_path(@book.id)
+    else
+      @books = Book.all
+      render :index
+    end
+    
+    
   end
   
-  def book = Book.find(params[:id])
+  def destroy
+    book = Book.find(params[:id])
     book.destroy
+    flash[:notice] = "Book was successfully created."
     redirect_to '/books'
   end
 
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ Rails.application.routes.draw do
   resources :books
   patch 'book/:id' => 'books#update', as: 'update_book'
   delete 'book/:id' => 'books#destroy', as: 'destroy_book'
+  get 'book/:id' => 'books#show', as: 'show_book'
 end
